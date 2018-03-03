d580f0a353d4b2a631285f35f362f8c00fde42c1
sgc88 <sehribancheng@yahoo.com>
2018-03-02 23:30:21 -0800

    adding an input filed where the user can set the winning score

---
+++ challenges.js
@@ -0,0 +1,97 @@
+
+var score, roundScore, activePlayer, gamePlaying, lastDice;
+init();
+
+
+
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+
+
+  if(gamePlaying){
+    //we need a random number
+    var dice = Math.floor(Math.random() * 6) +1;
+
+    // now we need the result
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = 'block';
+    diceDom.src = "dice-" + dice + ".png";
+    //save previous dice if it's 6
+    if(dice === 6 && lastDice === 6){
+      //player loses the score he has
+      score[activePlayer] = 0;
+      document.getElementById("score-" + activePlayer).innerHTML = score[activePlayer];
+
+    }else if(dice !== 1){
+      //add score
+      roundScore += dice;
+      document.querySelector("#current-" + activePlayer).textContent = roundScore;
+
+    }else{
+      //next player
+
+      nextPlayer();
+
+    }
+    lastDice = dice;
+
+  }
+
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function(){
+    if(gamePlaying){
+        // add current score to current player's total score
+        score[activePlayer] += roundScore;
+        // update the UI
+        document.getElementById("score-" + activePlayer).innerHTML = score[activePlayer];
+        // check if player won the GAME
+        if(score[activePlayer] >= 100){
+          document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+          document.querySelector(".dice").style.display = "none";
+          document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+          document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+          gamePlaying = false;
+        }else{
+          //next player
+          nextPlayer();
+        }
+    }
+});
+
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
+  roundScore = 0;
+    document.getElementById('current-0').textContent ="0";
+    document.getElementById('current-1').textContent ="0";
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+};
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init(){
+  gamePlaying = true;
+    score=[0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    //we also need a dice to display or indisplay after the players rolls it
+    document.querySelector(".dice").style.display = "none";
+    document.querySelector("#name-0").textContent = "PLAYER 1";
+    document.querySelector("#name-1").textContent = "PLAYER 2";
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
+
+
+
+    document.getElementById('score-0').textContent = "0";
+    document.getElementById('score-1').textContent = "0";
+    document.getElementById('current-0').textContent = "0";
+    document.getElementById('current-1').textContent = "0";
+
+
+}
